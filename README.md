564afd69500887750241be1819a088e4ea535cdf
AlexD <alexandredesoutter.pro@gmail.com>
2021-05-25 19:37:24 +0200

    ajout de formulaire

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # php basic
 
 Ce projet permet d'apprendre les bases de php.
+# php-basic-p7
---
+++ tag.php
@@ -0,0 +1,19 @@
+<?php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo 'lorem ipsum' ?></p>
+    <p><?= 'lorem ipsum' ?></p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi tempora eius itaque iste eveniet nihil omnis soluta hic debitis distinctio temporibus natus explicabo quae cum, nemo officiis odit facilis minima.</p>
+</body>
+</html>
+<?php
+
+// Pas de balise fermante si votre php termine le fichier
